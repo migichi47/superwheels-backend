71bb96db82bd97dd7094d98847246a79f870297b
Peter Migichi <petermigichi47@gmail.com>
2026-09-20 08:55:49 +0300

    use cors to authorize requests

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors";
 import products from "./data/products.js";
 
 const app = express();
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("This is the root route");
