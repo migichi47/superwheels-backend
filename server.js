f9d0bea5e03da77d8c14007b967cd458c5d64481
Peter Migichi <petermigichi47@gmail.com>
2026-09-20 13:35:16 +0300

    .

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ import cors from "cors";
 import products from "./data/products.js";
 
 const app = express();
-app.use(cors())
+app.use(cors());
+
+app.listen("3000", () => {
+  console.log("listening on port 3000");
+});
 
 app.get("/", (req, res) => {
   res.send("This is the root route");
@@ -11,6 +15,33 @@ app.get("/", (req, res) => {
 
 app.get("/api/products", (req, res) => {
   res.status(200).json(products);
+});
+
+// gets product with id using params
+app.get("/api/product/:id", (req, res) => {
+  const parsedId = parseInt(req.params.id);
+
+  try {
+    const findProduct = products.find((product) => product.id === parsedId);
+    if (!findProduct) res.status(404).json({ msg: "Product not found" });
+    res.status(200).send(findProduct);
+  } catch (error) {
+    res.status(400).json({ msg: "Bad request" });
+  }
+});
+
+// filters products with category
+app.get("/api/products/query", (req, res) => {
+  const searchQuery = req.query.category
+    .split(" ")
+    .join("")
+    .trim()
+    .toLowerCase();
+
+  const queryProducts = products.filter(
+    (product) => product.category.toLowerCase() === searchQuery,
+  );
+  res.status(200).send(queryProducts);
 });
 
 export default app;
